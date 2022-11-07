759271cec9cea711909ea0de2dd6c112afa83f15
Irbaganteng <gdriveirba@Gmail.com>
2022-11-07 09:47:49 +0700

    tugasnetwork

---
+++ soal Irba keren.py
@@ -0,0 +1,22 @@
+#Soal 1 suruh print hello world cok
+print("hello world")
+
+#Soal 2 aritmatika 2 angka aowkwk ejet
+print(2+2)
+print(10-2)
+print(2*2)
+print(10/2)
+#bonus pangkat
+print(4**2)
+
+#soal 3 
+a=10
+b=2
+print(int(a/b))
+print(float(a/b))
+
+
+#soal 4 cok
+usia=1000000
+kata='usia gwej:'
+print(kata,usia)
